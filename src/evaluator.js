faea5e9b1541ffa21494b534f7578bdc0b78e0bd
MystPi <blaunoah@gmail.com>
2022-03-01 10:45:57 -0500

    fix: typo

--- src/evaluator.js
+++ src/evaluator.js
@@ -96,7 +96,7 @@ specialForms.fun = (args, scope) => {
   let body = args[args.length - 1];
   let params = args.slice(0, args.length - 1).map(expr => {
     if (expr.type !== 'word') {
-      throw new SyntaxError('Paramter names must be words');
+      throw new SyntaxError('Parameter names must be words');
     }
     return expr.name;
   });
